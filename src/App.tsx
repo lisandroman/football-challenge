ed4d86b5ba1cc453c3f21bbd06aab694b2383de1
lisandroman <lisandromansilla.dg@gmail.com>
2022-10-04 11:00:48 +0300

    Install dependecies & 1st Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Navbar } from './components'
+import { Home } from './pages'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-     
-    </div>
+    <>
+     <Navbar />
+     <Home />
+    </>
   )
 }
 
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export interface HomeInterface {}
+
+const Home: React.FC<HomeInterface> = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
