54b34897c8d239b5a76e1439f6e12bf2861f8395
lisandroman <lisandromansilla.dg@gmail.com>
2022-10-04 09:54:29 +0300

    First Commit

---
+++ src/data/player.ts
@@ -0,0 +1,163 @@
+export const Player = [
+  {
+    id: '1',
+    name: 'Cristiano Ronaldo',
+    country: 'Portugal',
+    primeTeam: 'Real Madrid',
+    goals: '30'
+  },
+  {
+    id: '2',
+    name: 'Lionel Messi',
+    country: 'Argentina',
+    primeTeam: 'Barcelona FC',
+    goals: '100'
+  },
+  {
+    id: '3',
+    name: 'Pelé',
+    country: 'Brasil',
+    primeTeam: 'Santos',
+    goals: '50'
+  },
+  {
+    id: '4',
+    name: 'Romario',
+    country: 'Brasil',
+    primeTeam: 'Barcelona FC',
+    goals: '100'
+  },
+  {
+    id: '5',
+    name: 'Ferenc Puskás',
+    country: 'Hungary',
+    primeTeam: 'Real Madrid',
+    goals: '10'
+  },
+  {
+    id: '6',
+    name: 'Josef Bican',
+    country: 'Austria - Czech Republic',
+    primeTeam: 'Slavia Prague',
+    goals: '97'
+  },
+  {
+    id: '7',
+    name: 'Jimmy Jones',
+    country: 'Northern Ireland',
+    primeTeam: 'Glenavon FC',
+    goals: '21'
+  },
+  {
+    id: '8',
+    name: 'Gerd Müller',
+    country: 'Germany',
+    primeTeam: 'Bayern Munich',
+    goals: '10'
+  },
+  {
+    id: '9',
+    name: 'Eusébio',
+    country: 'Portugal',
+    primeTeam: 'Benfica',
+    goals: '10'
+  },
+  {
+    id: '10',
+    name: 'Joe Bambrick',
+    country: 'Ireland Soccer Team',
+    primeTeam: 'Linfield',
+    goals: '55'
+  },
+  {
+    id: '11',
+    name: 'Robert Lewandowski',
+    country: 'Poland',
+    primeTeam: 'Bayern Munich',
+    goals: '56'
+  },
+  {
+    id: '12',
+    name: 'Glenn Ferguson',
+    country: 'Northern Ireland',
+    primeTeam: 'Linfield',
+    goals: '50'
+  },
+  {
+    id: '13',
+    name: 'Zlatan Ibrahimović',
+    country: 'Sweden',
+    primeTeam: 'Inter Milan',
+    goals: '82'
+  },
+  {
+    id: '14',
+    name: 'Fernando Peyroteo',
+    country: 'Portugal',
+    primeTeam: 'Sporting Club',
+    goals: '23'
+  },
+  {
+    id: '15',
+    name: 'Jimmy McGrory',
+    country: 'Scotland',
+    primeTeam: 'Celtic Club',
+    goals: '55'
+  },
+  {
+    id: '16',
+    name: 'Uwe Seeler',
+    country: 'Germany',
+    primeTeam: 'Hamburger SV',
+    goals: '50'
+  },
+  {
+    id: '17',
+    name: 'Alfredo Di Stéfano',
+    country: 'Argentina',
+    primeTeam: 'Real Madrid',
+    goals: '45'
+  },
+  {
+    id: '18',
+    name: 'György Sárosi',
+    country: 'Hungary',
+    primeTeam: 'Ferencvaros',
+    goals: '81'
+  },
+  {
+    id: '19',
+    name: 'Luis Suárez',
+    country: 'Uruguay',
+    primeTeam: 'Barcelona FC',
+    goals: '35'
+  },
+  {
+    id: '20',
+    name: 'Roberto Dinamite',
+    country: 'Brasil',
+    primeTeam: 'Vasco da Gama',
+    goals: '56'
+  },
+  {
+    id: '21',
+    name: 'Hugo Sánchez',
+    country: 'Mexico',
+    primeTeam: 'Real Madrid',
+    goals: '56'
+  },
+  {
+    id: '22',
+    name: 'Imre Schlosser',
+    country: 'Hungary',
+    primeTeam: 'Ferencvaros',
+    goals: '56'
+  },
+  {
+    id: '23',
+    name: 'Franz Binder',
+    country: 'Austria',
+    primeTeam: 'Rapid Wien',
+    goals: '56'
+  }
+];
